c86a10b712befd186eecc2d81b5fb5fb6f87bb2e
SahithiNatte <sahithinatte@gmail.com>
2026-03-05 13:44:46 +0530

    Update page32a.js

--- page32a.js
+++ page32a.js
@@ -1,18 +1,22 @@
-
 let customers = [
-  { 
-    id: 1, 
-    name: "chancy",
+  {
+    id: 1,
+    name: "John",
     balance: 5000,
-    transactions: [] 
-},
-  { 
-    id: 2, 
-    name: "sisilia", 
-    balance: 5000, 
-    transactions: []
-   } ,
+    transactions: [],
+  },
+  {
+    id: 2,
+    name: "Amy",
+    balance: 5000,
+    transactions: [],
+  },
 ];
+
+function checkBalance(id) {
+  const customer = customers.find((customer) => customer.id === id);
+  console.log(customer)
+}
 
 function deposit(id, amount) {
   customers = customers.map((customer) => {
@@ -33,37 +37,42 @@ function deposit(id, amount) {
       return customer;
     }
   });
-  console.log("deposit successful");
 }
 
 function withdraw(id, amount) {
-  let customer = customers.find(customer=> customer.id === id);
-
-  if (customer && customer.balance >= amount) {
-    customer.balance -= amount;
-
-    customer.transactions.push({
-      type: "debit",
-      amount: amount,
-      date: Date.now()
-    });
-
-    console.log("Withdrawal successful");
-  } else {
-    console.log("Insufficient balance");
-  }
+  customers = customers.map((customer) => {
+    if (customer.id === id) {
+      if (customer.balance < amount) {
+        console.log("Insufficient Funds");
+        return customer
+      } else {
+        return {
+          ...customer,
+          balance: customer.balance - amount,
+          transactions: [
+            ...customer.transactions,
+            {
+              type: "debit",
+              amount: amount,
+              date: Date.now(),
+            },
+          ],
+        };
+      }
+    } else {
+      return customer;
+    }
+  });
 }
 
-function checkBalance(id) {
-  const customer = customers.find((customer) => customer.id === id);
-  console.log(customer);
+function showTransactions(id){
+const customer = customers.find(customer=>customer.id===id)
+console.table(customer.transactions)
 }
-function showTransactions(id) {
-  let customer = customers.find(customer => customer.id === id);
-    console.log(customer.transactions);
-  }
-
-deposit(2, 1000);
-withdraw(2, 500);
-checkBalance(2);
-showTransactions(2);
+deposit(1, 1500);
+checkBalance(1);
+deposit(1, 1000);
+checkBalance(1);
+withdraw(1,20000)
+checkBalance(1);
+showTransactions(1)
